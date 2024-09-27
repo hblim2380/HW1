32c0c80962f9d7a24192ea94d06619b6f9e85829
hblim2380 <dlagusqlssla@naver.com>
2024-09-27 17:54:13 +0900

    Feat: 덧셈뺄심py 추가

---
+++ addminus.py
@@ -0,0 +1,11 @@
+# 사용자로부터 두 정수 입력 받기
+num1 = int(input("첫 번째 정수를 입력하세요: "))
+num2 = int(input("두 번째 정수를 입력하세요: "))
+
+# 합과 차 계산
+sum_result = num1 + num2
+diff_result = num1 - num2
+
+# 결과 출력
+print("두 수의 합:", sum_result)
+print("두 수의 차:", diff_result)
