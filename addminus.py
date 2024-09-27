d531f0c2f4234b26293bb5409c95721fce0f1c0b
hblim2380 <dlagusqlssla@naver.com>
2024-09-27 18:18:23 +0900

    Feat: 두수곱 *2 추가

--- addminus.py
+++ addminus.py
@@ -10,8 +10,12 @@ diff_result = num1 - num2
 mul_result = num1 * num2
 div_result = num1 / num2
 
+# 두수의 곱에 2 곱하기
+mul2_result = num1 * num2 * 2
+
 # 결과 출력
 print("두 수의 합:", sum_result)
 print("두 수의 차:", diff_result)
 print("두 수의 곱:", mul_result)
 print("두 수의 나눗셈:", div_result)
+print("두 수의 곱 * 2:", mul2_result)
